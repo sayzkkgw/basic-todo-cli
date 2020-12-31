edaa4ce95704a72e571fcf538e0f251c93fa2bd8
yururin <sayzkkgw@gmail.com>
2020-12-31 03:16:29 +0000

    Create file open

---
+++ my_rust_test/src/main.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() {
+    let todo_file = File::open("todos.data").unwrap();
+    let todo_file_reader = BufReader::new(todo_file);
+    for todo_item in todo_file_reader.lines() {
+        if let Ok(item) = todo_item {
+            println!("[] - {}", item);
+        }
+    }
+}
