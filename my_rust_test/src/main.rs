ed1194d948c79c3229b4e9b00b835b5a1fd52457
yururin <sayzkkgw@gmail.com>
2021-01-01 12:46:41 +0900

    Add func to read a file

--- my_rust_test/src/main.rs
+++ my_rust_test/src/main.rs
@@ -3,11 +3,11 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 fn main() {
-    let todo_file = File::open("todos.data").unwrap();
-    let todo_file_reader = BufReader::new(todo_file);
+    let todo_file: File = File::open("todos.data").unwrap();
+    let todo_file_reader: BufReader<File> = BufReader::new(todo_file);
     for todo_item in todo_file_reader.lines() {
         if let Ok(item) = todo_item {
-            println!("[] - {}", item);
+            println!("[ ] - {}", item);
         }
     }
 }
